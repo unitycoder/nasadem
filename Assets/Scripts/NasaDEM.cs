6699bb9494397e511c98d3eb14ff7c4842e0d104
mika <support@unitycoder.com>
2021-02-20 01:03:54 +0200

    add project

---
+++ Assets/Scripts/NasaDEM.cs
@@ -0,0 +1,124 @@
+﻿// project https://github.com/unitycoder/nasadem
+// signup at at https://www.nasadem.xyz/ to get your API key
+
+using System;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NasaDEM : MonoBehaviour
+{
+    public string APIKey = "";
+    public Renderer r;
+
+    string url = "https://www.nasadem.xyz/api/v1/dem/";
+
+    public double latitude = 69.7795399;
+    public double longitude = 23.3147295;
+    [Range(5, 10)]
+    public int zoomLevel = 10;
+
+    public bool useCache = true;
+
+    [Tooltip("Cache folder inside StreamingAssets/")]
+    public string cacheFolder = "cache";
+    string cachePath;
+
+
+    IEnumerator Start()
+    {
+        if (string.IsNullOrEmpty(APIKey)) Debug.LogError("Missing API key..", gameObject);
+        if (r == null) Debug.LogError("Renderer is null..", gameObject);
+
+        cachePath = Path.Combine(Application.streamingAssetsPath, cacheFolder);
+        if (Directory.Exists(cachePath) == false)
+        {
+            Directory.CreateDirectory(cachePath);
+        }
+
+        Texture2D res = null;
+
+        // fetch data
+        yield return StartCoroutine(GetDEM(latitude, longitude, zoomLevel, value => res = value));
+
+        // show in mesh
+        r.material.mainTexture = res;
+    }
+
+    IEnumerator GetDEM(double lat, double lon, double height, System.Action<Texture2D> Result)
+    {
+        var x = long2tilex(lon, zoomLevel);
+        var y = lat2tiley(height, zoomLevel);
+
+        var req = url + zoomLevel + "/" + x + "/" + y + ".png";
+        Debug.Log("Request= " + req);
+
+        var cacheFileName = Path.Combine(cachePath, zoomLevel + "_" + x + "_" + y + ".png");
+        // TODO add caching check
+        if (useCache == true && File.Exists(cacheFileName))
+        {
+            Debug.Log("Loading cached file: " + cachePath);
+            // load cached
+            var fileBytes = File.ReadAllBytes(cacheFileName);
+            var tex = new Texture2D(2, 2);
+            tex.LoadImage(fileBytes);
+            Result(tex);
+        }
+        else // fetch it
+        {
+            using (UnityWebRequest www = UnityWebRequestTexture.GetTexture(req))
+            {
+                www.SetRequestHeader("Authorization", APIKey);
+
+                yield return www.SendWebRequest();
+
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    Debug.Log(www.error);
+                }
+                else
+                {
+                    var tex = DownloadHandlerTexture.GetContent(www);
+                    Result(tex);
+
+                    // save to cache
+                    if (useCache == true)
+                    {
+                        Debug.Log("Saving cached file: " + cacheFileName);
+                        var bytes = tex.EncodeToPNG();
+                        File.WriteAllBytes(cacheFileName, bytes);
+                    }
+                }
+            }
+        }
+    }
+
+
+    // https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#C.23
+    int long2tilex(double lon, int z)
+    {
+        return (int)(Math.Floor((lon + 180.0) / 360.0 * (1 << z)));
+    }
+
+    int lat2tiley(double lat, int z)
+    {
+        return (int)Math.Floor((1 - Math.Log(Math.Tan(ToRadians(lat)) + 1 / Math.Cos(ToRadians(lat))) / Math.PI) / 2 * (1 << z));
+    }
+
+    double tilex2long(int x, int z)
+    {
+        return x / (double)(1 << z) * 360.0 - 180;
+    }
+
+    double tiley2lat(int y, int z)
+    {
+        double n = Math.PI - 2.0 * Math.PI * y / (double)(1 << z);
+        return 180.0 / Math.PI * Math.Atan(0.5 * (Math.Exp(n) - Math.Exp(-n)));
+    }
+
+    double ToRadians(double degrees)
+    {
+        return degrees * 4.0 * Math.Atan(1.0) / 180.0;
+    }
+}
---
+++ Assets/Shaders/HeightmapDecode.shader
@@ -0,0 +1,76 @@
+﻿// quick vertex extrude shader test for nasadem data
+
+Shader "Unlit/Nasadem/HeightmapDecode"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "black" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+        Cull Off
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            // nasadem.xyz
+            // The API returns a PNG image, where each pixel encodes the height at that point. The formula to convert into meters is:
+            // let h = 256 * r + g - 32768;
+            // where r is the pixel value in the red channel, and g that of the green channel
+
+            float Decode(float2 tex)
+            {
+                return 256 * (tex.r*256) + (tex.g*256) - 32768;
+            }
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                
+                // read texture
+                float2 tex = tex2Dlod(_MainTex, float4(v.uv.xy, 0, 0)).rg;
+
+                // extrude
+                float height = Decode(tex);
+                v.vertex.y += height;
+
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = tex2D(_MainTex, i.uv);
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
